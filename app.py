c6f6eb401666bd931403046af666d459198b696e
Savio Dsouza <savy00713@gmail.com>
2022-07-02 19:23:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
+from prometheus_client import Summary
 import streamlit as st
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 
+title = st.text_area('Your text', 'Place your text here')
+
+
 stopWords = set(stopwords.words("english"))
-words = word_tokenize(text)
+words = word_tokenize(title)
 freqTable = dict()
+for word in words:
+    word = word.lower()
+    if word in stopWords:
+        continue
+    if word in freqTable:
+        freqTable[word] += 1
+    else:
+        freqTable[word] = 1
+
+sentences = sent_tokenize(title)
+sentenceValue = dict()
+
+for sentence in sentences:
+    for word, freq in freqTable.items():
+        if word in sentence.lower():
+            if sentence in sentenceValue:
+                sentenceValue[sentence] += freq
+            else:
+                sentenceValue[sentence] = freq
+
+sumValues = 0
+for sentence in sentenceValue:
+    sumValues += sentenceValue[sentence]
+
+
+average = int(sumValues / len(sentenceValue))
+
+summary = ''
+for sentence in sentences:
+    if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
+        summary += " " + sentence
+st.write(summary)
